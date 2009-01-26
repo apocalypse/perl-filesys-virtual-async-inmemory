1935eb383ab5f559f88d9c2700bf00ff011c92ad
Apocalypse <apoc@satellite.(none)>
2009-01-25 19:10:17 -0500

    copied example from SYNOPSIS

--- examples/simple.pl
+++ examples/simple.pl
@@ -3,25 +3,15 @@ use strict; use warnings;
 use Fcntl qw( :DEFAULT :mode );	# S_IFREG S_IFDIR, O_SYNC O_LARGEFILE etc
 
 # uncomment this to enable debugging
-#sub Filesys::Virtual::Async::Dispatcher::DEBUG { 1 }
+#sub Filesys::Virtual::Async::inMemory::DEBUG { 1 }
 
-use Filesys::Virtual::Async::Plain;
-use Filesys::Virtual::Async::Dispatcher;
+use Filesys::Virtual::Async::inMemory;
 
-# create the root filesystem
-my $rootfs = Filesys::Virtual::Async::Plain->new( 'root_path' => $ENV{'PWD'} );
-
-# create the extra filesystems
-my $tmpfs = Filesys::Virtual::Async::Plain->new( 'root_path' => '/tmp' );
-my $procfs = Filesys::Virtual::Async::Plain->new( 'root_path' => '/proc' );
-
-# put it all together
-my $vfs = Filesys::Virtual::Async::Dispatcher->new( 'rootfs' => $rootfs );
-$vfs->mount( '/tmp', $tmpfs ); 	# remember, this is relative so it would mount onto /home/apoc/tmp
-$vfs->mount( '/tmp/proc', $procfs );	 # remember, this is relative so it would mount onto /tmp/proc
+# create the filesystem
+my $vfs = Filesys::Virtual::Async::inMemory->new;
 
 # use $vfs as you wish!
-$vfs->readdir( '/', sub {	# should access the $rootfs object
+$vfs->readdir( '/', sub {	# should print out nothing
 	my $data = shift;
 	if ( defined $data ) {
 		foreach my $e ( @$data ) {
@@ -29,62 +19,46 @@ $vfs->readdir( '/', sub {	# should access the $rootfs object
 		}
 		print "end of listing for /\n";
 	} else {
-		print "no data in /\n";
+		print "error reading /\n";
 	}
-	part_one();
+	do_file_io();
 } );
 
-sub part_one {
-	$vfs->readdir( '/tmp/proc', sub {	# should access the $procfs object
-		my $data = shift;
-		if ( defined $data ) {
-			foreach my $e ( @$data ) {
-				print "entry in /tmp/proc -> $e\n";
-			}
-			print "end of listing for /tmp/proc\n";
-		} else {
-			print "error reading /tmp/proc\n";
-		}
-		part_two();
-	} );
+my $fh;
+sub do_file_io {
+	$vfs->mknod( '/bar', oct( '100644' ), 0, \&did_mknod );
 }
-
-sub part_two {
-	$vfs->open( '/tmp/proc/uptime', O_RDONLY, 0, sub {
-		my $fh = shift;
-		if ( defined $fh ) {
-			my $buf = "";
-			$vfs->read( $fh, 0, 1024, $buf, 0, sub {
-				if ( $_[0] > 0 ) {
-					print "read $_[0] bytes buf: <$buf>\n";
-					$vfs->close( $fh, sub {
-						print "close status: $_[0]\n";
-						part_three();
-					} );
-				} else {
-					print "FAILED TO READ: $!\n";
-				}
-			} );
-		} else {
-			print "FAILED TO OPEN: $!\n";
-		}
-	} );
-}
-
-sub part_three {
-	if ( ! $vfs->umount( '/tmp/proc' ) ) {
-		print "FAILED TO UMOUNT\n";
+sub did_mknod {
+	if ( $_[0] == 0 ) {
+		# write to it!
+		$vfs->open( '/bar', O_RDWR, 0, \&did_open );
 	} else {
-		$vfs->readdir( '/tmp/proc', sub {       # should access the $tmpfs object
-			my $data = shift;
-			if ( defined $data ) {
-				foreach my $e ( @$data ) {
-					print "entry in /tmp/proc -> $e\n";
-				}
-				print "end of listing for /tmp/proc\n";
-			} else {
-				print "error reading /tmp/proc\n";
-			}
-		} );
+		print "error mknod /bar\n";
+	}
+}
+sub did_open {
+	$fh = shift;
+	if ( defined $fh ) {
+		my $buf = "foobar";
+		$vfs->write( $fh, 0, length( $buf ), $buf, 0, \&did_write );
+	} else {
+		print "error opening /bar\n";
+	}
+}
+sub did_write {
+	my $wrote = shift;
+	if ( $wrote ) {
+		print "successfully wrote to /bar\n";
+		$vfs->close( $fh, \&did_close );
+	} else {
+		print "error writing to /bar\n";
+	}
+}
+sub did_close {
+	my $status = shift;
+	if ( $status == 0 ) {
+		print "successfuly closed fh\n";
+	} else {
+		print "error in closing fh\n";
 	}
 }
